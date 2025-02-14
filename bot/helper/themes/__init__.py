7f70dd5c51eb78869e5beddeac45cb5ea82c00a8
Kingunok <114995588+Kingunok@users.noreply.github.com>
2025-02-14 21:30:07 +0530

    Update __init__.py
    Signed-off-by: Kingunok <114995588+Kingunok@users.noreply.github.com>

--- bot/helper/themes/__init__.py
+++ bot/helper/themes/__init__.py
@@ -2,7 +2,8 @@
 from os import listdir
 from importlib import import_module
 from random import choice as rchoice
-from bot import config_dict, LOGGER
+from ..core.config_manager import Config
+from bot import LOGGER
 from bot.helper.themes import wzml_minimal
 
 AVL_THEMES = {}
@@ -12,7 +13,7 @@ for theme in listdir('bot/helper/themes'):
 
 def BotTheme(var_name, **format_vars):
     text = None
-    theme_ = config_dict['BOT_THEME']
+    theme_ = Config.BOT_THEME
 
     if theme_ in AVL_THEMES:
         text = getattr(AVL_THEMES[theme_].WZMLStyle(), var_name, None)
